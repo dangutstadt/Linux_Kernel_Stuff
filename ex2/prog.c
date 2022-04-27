8ce93e0d25aea5c1aecc922cfe4a46057cc196cf
Omri Tcherner <omimako@gmail.com>
2022-04-27 19:43:15 +0300

    added base file for ex2

---
+++ ex2/prog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <poll.h>
+#include <stdbool.h>
+
+int main(int argc, char *argv[]) 
+{
+    printf("---- Welcome to our server ----\n");
+
+    while(true)
+    {
+
+    }
+
+    printf("---- Server exiting... ----\n");
+    return 0;
+}
