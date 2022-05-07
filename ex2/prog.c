563ffe6cf8104e0e179f40f44026c10b57685f93
dangutstadt <dangutstadt1998@gmail.com>
2022-05-07 14:19:49 +0300

    finished this F*** S***

--- ex2/prog.c
+++ ex2/prog.c
@@ -1,3 +1,13 @@
+/**
+ * @file prog.c
+ * @author Dan Gutstadt 209321470 - Omri Tcherner 318946886
+ * @brief 
+ * @version 0.1
+ * @date 2022-05-07
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
@@ -12,12 +22,16 @@
 #include <netdb.h>
 
 #define PORT 8887
+#define D_PORT 80
 #define BUFFER_SIZE 4096
 #define FD_COUNT 2 
 #define LEAVE_COMMAND -1
 #define INVALID_ARGUMENT -2
 
 typedef struct sockaddr saddr;
+typedef struct sockaddr_in saddr_in;
+typedef struct in_addr in_addr;
+typedef struct pollfd pollfd;
 
 typedef struct _upload_obj {
     int file_fd;
@@ -28,53 +42,10 @@ typedef struct _download_obj {
     int id;
     char* url;
     int size_downloaded;
+    int socket_fd;
     int fd;
 } download_obj;
 
-int handle_show()
-{
-    return 0;
-}
-
-int handle_start(char* url)
-{
-    printf("[*] Starting to download from URL: %s\n", url);
-    return 0;
-}
-
-int handle_stop(char* id)
-{
-    printf("[*] Stopping download for ID: %s\n", id);
-    return 0;
-}
-
-int parse_command(char* buffer)
-{
-    char *token;
-
-    if(!strcmp(buffer, "leave\n"))
-    {
-        return LEAVE_COMMAND;
-    }
-    else if (!strcmp(buffer, "show\n"))
-    {
-        return handle_show();
-    }
-    else
-    {
-        token = strtok(buffer, " ");
-        if (!strcmp(token, "start"))
-        {
-            return handle_start(strtok(NULL, " "));
-        }
-        else if (!strcmp(token, "stop"))
-        {
-            return handle_stop(strtok(NULL, " "));
-        }
-        
-        return INVALID_ARGUMENT;
-    }
-}
 
 int main(int argc, char *argv[]) 
 {
@@ -85,12 +56,12 @@ int main(int argc, char *argv[])
     int byte_count = 0;
     int ret = 0;
     int uploads_count = 0;
+    int downloads_count = 0;
     int recv_fd = 0;
-    download_obj* objs = malloc(0);
-    upload_obj* uploads = malloc(0);;
-    struct pollfd *upload_fds = malloc(0);
-
-
+    download_obj* downloads = malloc(0);
+    upload_obj* uploads = malloc(0);
+    pollfd *upload_fds = malloc(0);
+    pollfd *downloads_fds = malloc(0);
 
     static const int NO_TIMEOUT = -1;
     static const int SERVE_COUNT = 5;
@@ -98,11 +69,9 @@ int main(int argc, char *argv[])
     printf("---- Welcome to our server ----\n");
     printf("[*] Initializing server...\n");
 
-    // stdin
     fds[0].fd = STDIN_FILENO;
     fds[0].events = POLLIN;
 
-    // server socket 
     fds[1].fd = socket(AF_INET, SOCK_STREAM, 0);
     fds[1].events = POLLIN;
 
@@ -122,38 +91,123 @@ int main(int argc, char *argv[])
         goto cleanup;
     }
 
-    // main server loop
     while(1)
     {
         poll(fds, FD_COUNT, 200);
 
-        // check if someone's ready to read
+        // Client
         if (fds[0].revents & POLLIN)
         {
             byte_count = read(STDIN_FILENO, buffer, BUFFER_SIZE - 1);
-
-            // add null terminator at the end
             buffer[byte_count] = '\0';
 
-            ret = parse_command(buffer);
-            if(ret == LEAVE_COMMAND)
+            char *token;
+
+            if(!strcmp(buffer, "leave\n"))
             {
                 printf("[*] Leaving!\n");
+                goto cleanup;
                 break;
             }
-            else if(ret == INVALID_ARGUMENT)
-            {  
-                printf("[ERROR] Invalid argument received\n");
+            else if (!strcmp(buffer, "show\n"))
+            {
+        
+                if (downloads_count == 0) {
+                    printf("There are not any files !\n");
+                } else {
+                    for (int index = 0; index < downloads_count; index++)
+                    {
+                        printf("%d %s %d\n", downloads[index].id, downloads[index].url, downloads[index].size_downloaded);
+                    }
+                }
+            }
+            else
+            {
+                token = strtok(buffer, " ");
+                if (!strcmp(token, "start"))
+                {
+                    char* url = strtok(NULL, " ");
+
+                    printf("[*] Starting to download from URL: %s\n", url);
+                    
+                    // Adding another struct of download to the downloads list
+                    downloads = realloc(downloads, (sizeof(download_obj) * (downloads_count+1)));
+                    downloads_fds = realloc(downloads_fds, sizeof(struct pollfd) * (downloads_count+1));
+
+                    char * filename = strtok(NULL, " ");
+                    filename[strlen(filename)-1] = '\0';  
+                    
+                    // Population the download structure
+                    downloads[downloads_count].id = downloads_count+1;
+                    
+                    downloads[downloads_count].url = malloc(strlen(url) + 1);
+                    strcpy(downloads[downloads_count].url, url);
+
+                    downloads[downloads_count].socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+                    downloads[downloads_count].fd = open(filename, O_WRONLY | O_CREAT, 0644);
+                    downloads[downloads_count].size_downloaded = 0;
+
+                    downloads_fds[downloads_count].fd = downloads[downloads_count].socket_fd;
+                    downloads_fds[downloads_count].events = POLLIN;
+
+                    url = malloc(strlen(downloads[downloads_count].url) + 1);
+                    strcpy(url, downloads[downloads_count].url);
+
+                    strtok(url, "/");
+                    char * hostname = strtok(NULL, "/");
+                    
+                    // Getting the port out of the url (if there is one)
+                    char* ch_port = (char *)strchr(hostname, ':');
+                    int port;
+                    if (ch_port) {
+                        *ch_port = '\0';
+                        ch_port++;
+                        port = atoi(ch_port);
+                    } else {
+                        port = D_PORT;
+                    }
+
+                    // Creating the socket connection to server via http 1.0 
+                    saddr_in connect_addr;
+                    connect_addr.sin_family = AF_INET;
+                    connect_addr.sin_port = htons(port);
+                    connect_addr.sin_addr = *(((struct in_addr **)gethostbyname(hostname)->h_addr_list)[0]);
+                    connect(downloads[downloads_count].socket_fd, (struct sockaddr*)&connect_addr, sizeof(connect_addr));
+                    char request[BUFFER_SIZE];
+                    sprintf(request, "GET /%s HTTP/1.0\r\nHost: %s\r\n\r\n\r\n", filename, hostname);
+                    send(downloads[downloads_count].socket_fd, request, strlen(request), 0);
+                    
+                    // Creating final url string
+                    char* final_url = malloc(strlen(filename) + strlen(downloads[downloads_count].url));
+                    sprintf(final_url, "%s%s", url, filename);
+                    free(downloads[downloads_count].url);
+                    downloads[downloads_count].url = final_url;
+
+                    downloads_count++;
+                }
+                else if (!strcmp(token, "stop"))
+                {
+                    char* id = strtok(NULL, " ");
+                    printf("[*] Stopping download for ID: %s\n", id);
+                    int index = atoi(id);
+                    close(downloads[index-1].fd);
+                    close(downloads[index-1].socket_fd);
+                } else {
+                    printf("[ERROR] Invalid argument received\n");
+                }   
             }
         }
+       
+        // Server
         else if (fds[1].revents & POLLIN)
         {
-            uploads = realloc(objs, sizeof(upload_obj) * uploads_count+1);
-            upload_fds = realloc(upload_fds, sizeof(struct pollfd) * uploads_count+1);
+            // Adding new upload
+            uploads = realloc(uploads, sizeof(upload_obj) * (uploads_count+1));
+            upload_fds = realloc(upload_fds, sizeof(struct pollfd) * (uploads_count+1));
 
             recv_fd = accept(fds[1].fd, (saddr*) &address, (socklen_t*) &addrlen);
             read(recv_fd, buffer, BUFFER_SIZE);
-            // buffer has the request
+    
 
             uploads[uploads_count].socket_fd = recv_fd;
             upload_fds[uploads_count].fd = recv_fd;
@@ -166,86 +220,104 @@ int main(int argc, char *argv[])
             uploads[uploads_count].file_fd = open(token, O_RDONLY);
 
             uploads_count+=1;
-
         }
 
+        // Handles the uploads (Server)
         poll(upload_fds, uploads_count, 200);
-
         for (int index = 0; index < uploads_count; index++)
         {
             int terminated = 0;
             memset(buffer, 0, BUFFER_SIZE);
 
-            // if termination was orderd
+            // Checks for connection termination (stop)
             if(upload_fds[index].revents & POLLHUP) {
                 terminated = 1;
             }
 
-            // Writing to socket
             if(terminated == 0 && upload_fds[index].revents & POLLOUT) {
-
-                // read bytes
-                int bytes_read = read(uploads[index].file_fd, &buffer, BUFFER_SIZE);
-
-                // if byte read - send
+                
+                // Sends data
+                int bytes_read = read(uploads[index].file_fd, &buffer, BUFFER_SIZE);        
                 if (bytes_read) {
                     send(uploads[index].socket_fd, &buffer, bytes_read, 0);
-                }
-
-                // else eof - close fds
-                else {
+                } else {
                     terminated = 1;
                 }
             }
-
-            // remove upload
+            
             if (terminated) {
-
                 close(uploads[index].file_fd);
                 close(uploads[index].socket_fd);
-
-                // remove upload from uploads
-                for(int j = index; j < uploads_count; j++) {
-                    uploads[j] = uploads[j+1];
-                    upload_fds[j] = upload_fds[j+1];
+                 
+                // Removes terminated record from uploads
+                for(int jndex = index; jndex < uploads_count; jndex++) {
+                    uploads[jndex] = uploads[jndex+1];
+                    upload_fds[jndex] = upload_fds[jndex+1];
                 }
-                if (--uploads_count == 0) {
-                    free(uploads);
-                    uploads = malloc(0);
-                    free(upload_fds);
-                    upload_fds= malloc(0);
-                } else {
-                    uploads = realloc(uploads, sizeof(upload_obj) * uploads_count);
-                    upload_fds = realloc(upload_fds, sizeof(struct pollfd) * uploads_count);
-                }
-                
+                                
+                uploads = realloc(uploads, sizeof(upload_obj) * uploads_count);
+                upload_fds = realloc(upload_fds, sizeof(struct pollfd) * uploads_count);
                 index--;
             }
         }
         
+        // Handles the downloads for (Client)
+        poll(downloads_fds, downloads_count, 200);
+        for (int index = 0; index < downloads_count; index++) {
 
-        // zero out buffer for next poll
+            int terminated = 0;
+            memset(buffer, 0, BUFFER_SIZE);
+            
+            // In case the download is compleate
+            if(fcntl(downloads_fds[index].fd, F_GETFD) != 0) {
+                terminated = 1;
+            }
+
+    
+            if (terminated == 0 && downloads_fds[index].revents & POLLIN) {
+
+                // Getting data from server
+                int bytes_read = recv(downloads[index].socket_fd, &buffer, BUFFER_SIZE, 0);
+                if (bytes_read == 0) {
+                    terminated = 1;
+                }  else {
+                    write(downloads[index].fd, &buffer, bytes_read);
+                    downloads[index].size_downloaded += bytes_read;
+                }
+            }
+
+            if (terminated) {
+                printf("[*] Finished download %s\n", downloads[index].url);
+                close(downloads[index].fd);
+                close(downloads[index].socket_fd);
+                free(downloads[index].url);
+
+                // Removing relevant record
+                for(int jndex = index; jndex < downloads_count; jndex++) {
+                    downloads[jndex] = downloads[jndex+1];
+                    
+                    // Importent - we must do that to sincronize the id with the place of the records in all the lists
+                    downloads[jndex].id -=1;
+                    downloads_fds[jndex] = downloads_fds[jndex+1];
+                }
+                downloads = realloc(downloads, sizeof(download_obj) * --downloads_count);
+                downloads_fds = realloc(downloads_fds, sizeof(pollfd) * downloads_count);
+                index--;
+            }
+        }
+        
+        // Makes sure the buffer is 
         memset(buffer, 0, BUFFER_SIZE);
     }
+
 
 cleanup:
     printf("[*] Starting cleanup...\n");
 
-    if(fds[1].fd != -1)
-    {
-        close(fds[1].fd);
-    }
-
-    if(objs != NULL)
-    {
-        free(objs);
-    }
-    
-    if (recv_fd > STDERR_FILENO)
-    {
-        close(recv_fd);
-    }
-
+    free(uploads);
+    free(upload_fds);
+    free(downloads);
+    free(downloads_fds); 
 
     printf("---- Server exiting... ----\n");
     return 0;
